9ae259ccf49793194295315f389e6c2672370177
Liam Galvin <liam@infracost.io>
2026-03-04 17:20:47 +0000

    add shas for tyop release

--- Casks/tyop.rb
+++ Casks/tyop.rb
@@ -3,12 +3,12 @@ cask "tyop" do
 
   on_arm do
     url "https://github.com/liamg/tyop/releases/download/v#{version}/tyop-darwin-arm64.tar.gz"
-    sha256 "REPLACE_WITH_ARM64_SHA256"
+    sha256 "bebd24be4f3adc6a8c9781d7a5a0e0ec658bce03273513ce5355e7ad7abeaa66"
   end
 
   on_intel do
     url "https://github.com/liamg/tyop/releases/download/v#{version}/tyop-darwin-amd64.tar.gz"
-    sha256 "REPLACE_WITH_AMD64_SHA256"
+    sha256 "1be32ef23ddeeb27ffd8637305c51145c9761f10cd4602cce34ce569f51f97c1"
   end
 
   name "tyop"
